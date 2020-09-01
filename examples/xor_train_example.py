8eae7e9a517bae05a531aaa5025686b0d89601cb
DasAnish <ad945@cam.ac.uk>
2020-09-01 17:13:35 +0530

    Minor change to xor

--- examples/xor_train_example.py
+++ examples/xor_train_example.py
@@ -1,6 +1,8 @@
 from layer import *
 from clobject import *
 from neuralnet import NeuralNet
+import os
+os.chdir('..')
 
 
 xys = []
@@ -8,8 +10,6 @@ nums = [0, 1, 3, 2, 6, 14, 10, 11, 9, 8, 12, 4, 5, 7, 15, 13]
 num2 = [0, 1, 5, 4, 12, 8, 9, 13, 15, 11, 10, 14, 6, 7, 3, 2]
 num3 = [0, 4, 12, 8, 9, 13, 5, 1, 3, 7, 15, 11, 10, 14, 6, 2]
 num4 = [0, 8, 10, 2, 3, 7, 6, 4, 5, 1, 9, 13, 12, 14, 15, 11]
-num5 = [i for i in range(16)]
-np.random.shuffle(num5)
 for i in range(16):
     x = np.binary_repr(i, 4)
     x = [int(j) for j in x]
@@ -28,13 +28,15 @@ nn = NeuralNet(
 
 
 nn.predict_values(xys)
-nn.train(
+fun = lambda: nn.train(
     xys,
     batch_size=len(xys),
-    num_epochs=4500,
+    num_epochs=100,
     print_every=100,
-    predict_after_every_batch=True
+    predict_after_every_batch=False
 )
+
+fun()
 
 
 print("**************FINAL PREDICTION***************************")
